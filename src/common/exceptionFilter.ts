5f45c1442b6144bc29bce4f0fe624d5737de2d4c
ellie <ellie@hyper-cloud.kr>
2024-02-18 23:34:50 +0900

    FIX: error exception filter 수정

--- src/common/exceptionFilter.ts
+++ src/common/exceptionFilter.ts
@@ -19,7 +19,6 @@ export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-    const headers = ctx.getRequest().headers;
     const message = exception.toString();
     const status =
       exception instanceof HttpException
@@ -27,31 +26,10 @@ export class AllExceptionsFilter implements ExceptionFilter {
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
     response.errorStack = exception.stack;
-    if (exception instanceof HttpException) {
-      let desc;
-      const res = exception.getResponse() as RequestExceptionResponse;
 
-      if (res.desc) {
-        desc = Array.isArray(res.desc) ? res.desc : [res.desc];
-      } else {
-        desc = Array.isArray(res.message) ? res.message : [res.message];
-      }
-
-      return response.status(status).json({
-        status: status,
-        message: message,
-        error: { reqId: headers.reqId, code: res.code || String(status), desc },
-      });
-    } else {
-      return response.status(status).json({
-        status: status,
-        message: message,
-        error: {
-          reqId: headers.reqId,
-          code: '0000',
-          desc: ['예기치 못한 오류가 발생했습니다.'],
-        },
-      });
-    }
+    return response.status(status).json({
+      status: status,
+      message: message,
+    });
   }
 }
